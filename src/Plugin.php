b4f4f99281af230f64d3a86912b6879c4f319e56
Aubrey Portwood <code@aubreypwd.com>
2020-07-08 10:18:21 -0600

    Fix plugin.

---
+++ src/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace aubreypwd\Composer\Uninstall;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function getCapabilities()
+    {
+        return array(
+            'Composer\Plugin\Capability\CommandProvider' => 'aubreypwd\Composer\Uninstall\CommandProvider',
+        );
+    }
